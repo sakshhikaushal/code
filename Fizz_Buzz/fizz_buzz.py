b8954a64505566ab001ded44230c444f4aeb897b
Sakshhi Kaushal <sakshhikaushal@gmail.com>
2025-12-15 17:21:06 +0530

    added fizz buzz code

---
+++ Fizz_Buzz/fizz_buzz.py
@@ -0,0 +1,15 @@
+"""
+If number is only divisible by 3, print fizz
+if number is only divisible by 5, print buzz
+if number is divisble by both 3 & 5, print fizzbuzz
+Else any of the condition is not satisfied, print Normal
+"""
+# a = int(input("enter number: "))
+# if a % 3 == 0 and a % 5 == 0:
+#     print("fizzbuzz")
+# elif a % 3 == 0:
+#     print("fizz")
+# elif a % 5 == 0:
+#     print("buzz")
+# else:
+#     print("Normal")

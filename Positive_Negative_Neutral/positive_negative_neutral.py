365fd0f31eeb34714fc97b2205aeb1080ffc050c
Sakshhi Kaushal <sakshhikaushal@gmail.com>
2025-12-15 17:24:15 +0530

    added positive, negative, neutral code

---
+++ Positive_Negative_Neutral/positive_negative_neutral.py
@@ -0,0 +1,20 @@
+"""
+User will give number whether the number is positive, negative or neutral
+"""
+# a = 57
+# b = 100
+# if b > a:
+#     total_number_diff = b - a
+# elif b == a:
+#     total_number_diff = 0
+# else:
+#     total_number_diff = a - b
+# print(total_number_diff)
+
+# a = int(input("enter number: "))
+# if a > 0:
+#     print("positive")
+# elif a == 0:
+#     print("neutral")
+# else:
+#     print("negative")

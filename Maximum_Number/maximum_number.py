52e4a4ca23e1684182a8bed6b7677073fbb642f3
Sakshhi Kaushal <sakshhikaushal@gmail.com>
2025-12-15 17:22:28 +0530

    added maximum number code

---
+++ Maximum_Number/maximum_number.py
@@ -0,0 +1,30 @@
+"""
+Maximum of three numbers
+"""
+# a = 296
+# b = 197
+# c = 198
+# maxx = 0
+# if a > b:
+#     maxx = a
+# else:
+#     maxx = b
+# if c > maxx:
+#     print(c)
+# else:
+#     print(maxx)
+
+# a = 96
+# b = 297
+# c = 198
+
+# if a > b:
+#     if a > c:
+#         print(a)
+#     else:
+#         print(c)
+# else:
+#     if b > c:
+#         print(b)
+#     else:
+#         print(c)
